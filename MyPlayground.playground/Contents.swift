fa16cd113abbf4645c3b80f4a19042df1733f3c2
Denis Abramov <denis.m.abramov@mail.ru>
2020-10-27 22:15:29 +0300

    Add myPlayground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+var markaOfCar = "BMW"
+var year: Int = 2010
+var weight = 1450.5
+
+markaOfCar = "UAZ"
+
+print(markaOfCar)
+print("Prosto text")
+print("My auto \(markaOfCar)")
+
+markaOfCar = "Tayota"
+print(markaOfCar)
+
+var lastName = "Abramov"
+lastName = "Abramov1"
+
+// coment1
+/*1
+ 2
+ 3
+ */
